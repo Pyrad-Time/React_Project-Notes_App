2731f49eb9b68b3c3faa2e170107758eec274d3b
Pyrad-Time <marcos0fataak@gmail.com>
2026-05-13 09:44:15 -0300

    add component structure, render note data via propos and remove item button

---
+++ front-end/src/components/NoteCard/NoteCard.jsx
@@ -0,0 +1,13 @@
+export default function NoteCard(props) {
+    const note = props.note
+    return (
+        
+        <section>
+            <div className="card__container">
+                <h2>{note.title}</h2>
+                <p>{note.content}</p>
+                <button onClick={props.removeNote}>Remove Card</button>
+            </div>
+        </section>
+    )
+}
