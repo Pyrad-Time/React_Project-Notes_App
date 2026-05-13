a0d8fdeb35f302e6c1a16848cf8c401264f1893b
Pyrad-Time <marcos0fataak@gmail.com>
2026-05-13 09:39:06 -0300

    feat(header): add header component structure

---
+++ front-end/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header className="header__container">
+            <h1 className="header__title">Note App</h1>
+            <p className="header__subtitle">Created By: Pyrad-Time</p>
+        </header>
+    )
+}
+
+export default Header
