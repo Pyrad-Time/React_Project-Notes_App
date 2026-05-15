3b207d2822805d6d023c5a9566dd8d83670053bc
Pyrad-Time <marcos0fataak@gmail.com>
2026-05-15 10:37:24 -0300

    feat(api): implement in-memory notes CRUD

--- back-end/server.js
+++ back-end/server.js
@@ -6,13 +6,15 @@ const app = express();
 app.use(express.json());
 // Permite o backend entender JSON no corpo das requisições. 
 
-const notes = [
+let notes = [
     {
         id: 1,
         title: "First Note",
         content: "Note test"
     }
 ]
+
+let nextId = 2
 
 
 app.get("/console", (req, res) => {
@@ -29,16 +31,38 @@ app.post("/notes", (req, res) => {
     // desestrutura e define valor da variável
 
     const newNote = {
-        id: notes.length + 1,
+        id: nextId,
         title,
         content
     };
+    nextId++
+
 
     notes.push(newNote);
 
     res.status(201).json(newNote);
     
 });
+
+app.delete("/notes/:id", (req, res) => {
+    // :id é um espaço variável na URL
+
+    const id = Number(req.params.id);
+    // req.params.id vem como string,e ntão convertemos para número
+
+    const noteExist = notes.some((note) => note.id === id);
+    // Verifica se a nota existe
+
+    if(!noteExist)  
+        return res.status(404).json({ message: "Note not found"})
+    // Caso a nota não exista, retorne 404 com a mensagem 
+
+    notes = notes.filter((note) => note.id !== id);
+    // Filtra e retorna para o estado 
+
+    res.json({ message: "note deleted successfully"});
+    //
+})
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
