3a420b8ae5a5e351ca5df88e20cd59786ea6c160
Pyrad-Time <marcos0fataak@gmail.com>
2026-05-18 09:12:21 -0300

    feat(backend): configure CORS origin

--- back-end/server.js
+++ back-end/server.js
@@ -4,12 +4,15 @@
     Conectar rotas
     Ligar servidor
  */
-
 const express = require("express");
 const notesRoutes = require("./routes/notesRoute");
+const cors = require("cors");
 
 const app = express();
 // Cria a aplicação back end
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
 
 app.use(express.json());
 // Permite o backend entender JSON no corpo das requisições. 
