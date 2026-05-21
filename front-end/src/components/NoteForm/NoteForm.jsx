d3e0c15c4b65210f8dbd61e37c79000d4b5bcd5d
Pyrad-Time <marcos0fataak@gmail.com>
2026-05-21 09:25:23 -0300

    style(noteForm): create responsive form structure

--- front-end/src/components/NoteForm/NoteForm.jsx
+++ front-end/src/components/NoteForm/NoteForm.jsx
@@ -1,3 +1,4 @@
+import "./NoteForm.css"
 import { useState } from "react"
 
 export default function NoteForm(props) {
@@ -6,15 +7,22 @@ export default function NoteForm(props) {
 
     return (
         
-        <section>
-            <form onSubmit={(e) => {
+        <section className="noteForm__section">
+            <form 
+                className="noteForm__container"
+                onSubmit={(e) => {
                 e.preventDefault()
                 props.onAddNote(cardTitle, cardData)
 
                 setCardTitle('')
                 setCardData('')
             }}>
+                <label htmlFor="form-input" className="noteForm__label">
+                    Title
+                </label>
+
                 <input  
+                    className="noteForm__input"
                     type="text"
                     placeholder="Add your note..."
                     id="form-input"
@@ -22,18 +30,19 @@ export default function NoteForm(props) {
                     onChange={(e) => setCardTitle(e.target.value)}
                 />
                 
-                <label htmlFor="textarea-content">Content:</label>
+                <label htmlFor="textarea-content" className="noteForm__label">Content</label>
 
                 <textarea 
+                    className="noteForm__textarea"
                     name="content" 
                     id="textarea-content" 
-                    cols="30" 
-                    rows="10"
+                    rows="8"
                     value={cardData}
                     onChange={(e) => setCardData(e.target.value)}
                 ></textarea>
 
                 <button 
+                    className="noteForm__button"
                     type="submit"
                 >Add</button>
         </form >
