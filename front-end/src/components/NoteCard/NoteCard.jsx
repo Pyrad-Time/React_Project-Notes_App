33fad8fbec39596339aa8c91f5f406848b30a8d4
Pyrad-Time <marcos0fataak@gmail.com>
2026-05-21 09:54:20 -0300

    style(noteCard): create post-it card layout

--- front-end/src/components/NoteCard/NoteCard.jsx
+++ front-end/src/components/NoteCard/NoteCard.jsx
@@ -1,14 +1,20 @@
+import "./NoteCard.css"
+
 export default function NoteCard(props) {
     const note = props.note
     return (
         
-        <section>
-            <div className="card__container">
-                <h2>{note.title}</h2>
-                <p>{note.content}</p>
-                <button onClick={props.removeNote}>Remove Card</button>
-            </div>
-            
-        </section>
+        <article className="noteCard__container">
+                <h2 className="noteCard__title">{note.title}</h2>
+
+                <p className="noteCard__content">{note.content}</p>
+
+                <button 
+                    className="noteCard__button"
+                    type="button"
+                    onClick={props.removeNote}
+                    >Remove Card
+                </button>            
+        </article>
     )
 }
