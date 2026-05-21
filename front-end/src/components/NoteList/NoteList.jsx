4169f07e1032c2fb45f2a5013836bea7bdd3ebfb
Pyrad-Time <marcos0fataak@gmail.com>
2026-05-21 10:16:20 -0300

    style(noteList): add list structure and css classes

--- front-end/src/components/NoteList/NoteList.jsx
+++ front-end/src/components/NoteList/NoteList.jsx
@@ -5,8 +5,27 @@ export default function NoteList(props) {
     const onRemoveNote = props.onRemoveNote
 
     return (
-        notes.map((note) => {
-           return <NoteCard key={note.id} note={note} removeNote={() => onRemoveNote(note.id)}/>
-        })
+        <section className="noteList__container">
+            <h2 className="noteList__title">You Notes</h2>
+
+            {props.notes.length === 0 ? (
+                <p className="noteList__empty">No notes created yet.</p>
+            ) : (
+                <div className="noteList__grid">
+                    {
+                        notes.map((note) => {
+                            return (<NoteCard 
+                                key={note.id}
+                                note={note}
+                                removeNote={() => onRemoveNote(note.id)}
+                            />)
+                        })
+                    }
+                </div>
+            )
+
+            }
+        </section>
+        
     )
 }
