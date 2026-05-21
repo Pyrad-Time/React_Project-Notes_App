746deec81e7da69501a2f5b787fd766407a31333
Pyrad-Time <marcos0fataak@gmail.com>
2026-05-20 23:33:11 -0300

    Style(header): add responsive and accessible header

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -1,8 +1,18 @@
+import "./Header.css";
+import headerTitleImg from "../../assets/notes-header.png"
+
 function Header() {
     return (
         <header className="header__container">
-            <h1 className="header__title">Note App</h1>
-            <p className="header__subtitle">Created By: Pyrad-Time</p>
+            <h1 className="header__title sr-only">Notes App</h1>
+            <p className="header__subtitle sr-only">Created By: Pyrad-Time</p>
+
+            <img 
+                src={headerTitleImg}
+                alt="" 
+                aria-hidden="true"
+                className="header__img"
+            />
         </header>
     )
 }
